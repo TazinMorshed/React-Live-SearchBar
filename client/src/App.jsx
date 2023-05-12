52f648c16bc28d6d4363daa173bb00a9daca6d55
Tazin <45015333+TazinMorshed@users.noreply.github.com>
2023-05-12 17:40:23 +0600

    setup backend and fetch data

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import "./app.css";
+import Table from "./Table";
+import axios from "axios";
+
+function App() {
+  const [query, setQuery] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await axios.get(`http://localhost:3000?q=${query}`);
+      console.log(res);
+      setData(res.data);
+    };
+    if (query.length === 0 || query.length > 1) fetchUsers();
+  }, [query]);
+
+  return (
+    <div className="app">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <Table data={data} />
+    </div>
+  );
+}
+
+export default App;
