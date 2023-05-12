c8dfabf0e709b75f68452b0b013063c3f019de44
Tazin <45015333+TazinMorshed@users.noreply.github.com>
2023-05-12 16:09:28 +0600

    multi-search

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
 import "./app.css";
+import Table from "./Table";
 import { Users } from "./users";
 
 function App() {
   const [query, setQuery] = useState("");
+
+  const search = (data) => {
+    return data.filter(
+      (item) =>
+        item.first_name.toLowerCase().includes(query) ||
+        item.last_name.toLowerCase().includes(query) ||
+        item.email.toLowerCase().includes(query)
+    );
+  };
+
   return (
     <div className="app">
       <input
@@ -12,15 +23,7 @@ function App() {
         placeholder="Search..."
         onChange={(e) => setQuery(e.target.value)}
       />
-      <ul className="list">
-        {Users.filter((user) =>
-          user.first_name.toLowerCase().includes(query)
-        ).map((user) => (
-          <li className="listItem" key={user.id}>
-            {user.first_name}
-          </li>
-        ))}
-      </ul>
+      <Table data={search(Users)} />
     </div>
   );
 }
---
+++ src/Table.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/prop-types */
+const Table = ({ data }) => {
+  return (
+    <>
+      <table>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Email</th>
+          </tr>
+          {data.map((item) => (
+            <tr key={item.id}>
+              <td>{item.first_name}</td>
+              <td>{item.last_name}</td>
+              <td>{item.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default Table;
